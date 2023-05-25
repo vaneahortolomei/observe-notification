69bec8444f1debc50cc9d060865833f82b1dc549
Ваня Хортоломей <hortolomei1989@gmail.com>
2023-05-25 16:19:00 +0300

    add config in vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import {defineConfig} from "vite";
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                sw: './sw.js'
+            },
+            output: {
+                entryFileNames: '[name].[hash].js'
+            }
+        }
+    },
     server: {
         port: 3050,
     },
